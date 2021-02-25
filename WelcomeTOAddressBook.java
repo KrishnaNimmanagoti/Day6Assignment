7e101afada9639a14edb9a9e0f7d2d9e2bca765a
KrishnaNimmanagoti <78581769+KrishnaNimmanagoti@users.noreply.github.com>
2021-02-25 06:02:11 +0530

    Welcome to AddressBookSystem

---
+++ WelcomeTOAddressBook.java
@@ -0,0 +1,17 @@
+package day6Assignment.addressBookSystem;
+
+public class WelcomeTOAddressBook {
+	
+	public static void display() {
+		
+		System.out.println("Welcome to AddressBook Program");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		display();
+		
+	}
+
+}
